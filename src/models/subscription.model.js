d7235623bfdd125347b20d1a500695042a366279
Pratham R Vyas <prathamrvyas@gmail.com>
2026-07-26 22:39:57 +0530

    Add subscription model with user account management endpoints

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { User } from "./user.model";
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,                 //one who is subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId,                 //one to whom 'subscriber' is subscribing       
+        ref: "User"
+    },
+    
+}, {timestamps: true})
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
